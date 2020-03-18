90e062665c8d6c1d109419987a0c599ed1f7ee16
Sergey Evstegneiev <go@123automate.it>
2020-03-18 19:02:08 +0100

    sinatra on port 9980

--- docker/api/docker-entrypoint.sh
+++ docker/api/docker-entrypoint.sh
@@ -3,5 +3,5 @@
 set -e
 BUNDLE_IGNORE_CONFIG=1 bundle install
 
-ruby -r/app/spec/support/fake_api.rb -eFakeAPI.start!
+ruby -r/app/spec/support/fake_api.rb -e"FakeAPI.set :bind, '0.0.0.0'; FakeAPI.set :port, 9980; FakeAPI.start!"
 
